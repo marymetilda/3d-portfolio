e2ea1e1e1635ed5c2a48aae084be9ae9f45064a3
marymetilda <metildaphy@gmail.com>
2025-02-18 12:01:05 +0530

    home page with 3d models

---
+++ src/models/Sky.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import { useGLTF } from "@react-three/drei";
+
+import skyScene from "../assets/3d/sky.glb";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
+const Sky = ({ isRotating }) => {
+  const skyRef = useRef();
+
+  const sky = useGLTF(skyScene);
+
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyRef.current.rotation.y += 0.15 * delta;
+    }
+  });
+
+  return (
+    <mesh ref={skyRef}>
+      <primitive object={sky.scene} />
+    </mesh>
+  );
+};
+
+export default Sky;
